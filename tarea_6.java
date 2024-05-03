67d5df80fe611f71aa4d524382430ef7efffa978
eduard_cupcakes <161329071+cupcakesweet@users.noreply.github.com>
2024-05-03 14:48:03 -0500

    Add files via upload

---
+++ tarea_6.java
@@ -0,0 +1,15 @@
+package com.mycompany.ciclos_cristian;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class tarea_6 {
+     public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        int n = -1;
+        do {
+            System.out.print("Ingrese un numero: ");
+            n = input.nextInt();
+        } while (n!=0);
+    }
+}
