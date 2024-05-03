16a11f1eebd0380e47dd0e57e8cf91a2ff67d230
eduard_cupcakes <161329071+cupcakesweet@users.noreply.github.com>
2024-05-03 15:17:26 -0500

    Add files via upload

--- tarea_2.java
+++ tarea_2.java
@@ -1,18 +1,28 @@
-package com.mycompany.estructura_cristian;
-
+package com.mycompany.ciclos_cristian;
+import java.util.Scanner;
+import java.io.*;
 public class tarea_2 {
-    public static void main(String[] args) {
+     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
-        System.out.print("Ingrese un numero: ");
-        int numero = input.nextInt();
-        if (numero>0) {
-            System.out.println("El numero es mayor a 0");
-        } else {
-            if (numero < 0) {
-                System.out.println("El numero es menor a 0");
-            } else {
-                System.out.println("El numero es igual a 0");
-            }
+        System.out.print("Ingrese el valor de n: ");
+        int n = input.nextInt(), i=0; //Se inicializan la variable n y el acomulador i//
+        do { //Se inicia el ciclo//
+            System.out.println(i);
+            i++; //En cada interación se le suma 1 a i//
+        } while (i<=n); //Se repitirá el ciclo siempre que i sea igual o menor que n//
+        
+        System.out.print("Ingrese el valor de n: ");
+        n = input.nextInt();
+        i = 0;
+        while (i <= n) { //Se ejecutará el ciclo siempre que i sea menor o igual a n//
+            System.out.println(i);
+            i++; //se le suma 1 a i//
+        }
+
+        System.out.print("Ingrese el valor de n: ");
+        n = input.nextInt();
+        for (i=0;i<=n;i++) { //Al iniciar el ciclo, se define i como 0, se ejecutará el ciclo siempre que la condición sea correcta y al final se le suma 1 a i//
+            System.out.println(i);
         }
     }
 }
