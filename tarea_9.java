a57577797bb88748dd30cb72823d76966f784849
eduard_cupcakes <161329071+cupcakesweet@users.noreply.github.com>
2024-05-03 14:54:37 -0500

    Add files via upload

---
+++ tarea_9.java
@@ -0,0 +1,13 @@
+package com.mycompany.ciclos_cristian;
+
+public class tarea_9 {
+     public static void main(String[] args) {
+        int i = 1;
+        while (i<=10) {
+            System.out.println(i+" x 5 = "+(i++*5));
+        }
+    }
+}
+/*
+Cree un algoritmo que imprima los primeros  10 resultados de la tabla del 5, usando para esto el ciclo while.
+*/
